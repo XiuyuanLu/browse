d4c73b1bce29ff4e3854017285d56321aeb3aec0
Simon Allen <simon@simonallen.org>
2019-09-12 16:52:26 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sallybrowse",
+    version="1.0.0",
+    author="Simon Allen",
+    author_email="author@example.com",
+    description="Easy to use web-based file/directory viewer.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/garfunkel/sallybrowse",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
