aa7948ffffedc10995ae013e6a2dec5a6fc36187
Simon Allen <simon@simonallen.org>
2019-09-13 03:43:49 +1000

    add reqs

--- setup.py
+++ setup.py
@@ -19,4 +19,13 @@ setuptools.setup(
 		"Operating System :: OS Independent",
 	],
 	python_requires='>=3.6',
+	install_requires=[
+		"flask",
+		"chardet",
+		"pygments",
+		"textblob",
+		"humanize",
+		"python-magic",
+		"git+https://gitlab.com/garfunkel/python-zipstream.git"
+	]
 )
