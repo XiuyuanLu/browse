923cc8adfb763e8025b9e39861e12a18eef8eada
Nick1221 <ninjas1221@gmail.com>
2019-12-23 16:31:17 +1100

    Fixed encoding decoding on s3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="sallybrowse",
-	version="1.1.9.3",
+	version="1.2.0",
 	author="Simon Allen/Nick Leung",
 	author_email="author@example.com",
 	description="Easy to use web-based file/directory viewer.",
