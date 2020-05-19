e848f139d81eb2c02535b5b97ff5e236840efb62
Nick1221 <ninjas1221@gmail.com>
2020-05-19 17:44:33 +1000

    ty harry

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="sallybrowse",
-	version="1.2.8.9.01",
+	version="1.2.8.9.02",
 	author="Simon Allen/Nick Leung",
 	author_email="author@example.com",
 	description="Easy to use web-based file/directory viewer.",
